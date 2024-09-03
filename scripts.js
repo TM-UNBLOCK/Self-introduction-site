fd96fff750dc2dd22263f853f4a37e6637d19252
TM-UNBLOCK <MaipuMacrothorax@gmail.com>
2024-09-03 20:51:12 +0900

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,13 +1,14 @@
+// JavaScriptによるページ遷移時のエフェクトなど
 window.addEventListener('scroll', function() {
-    const sections = document.querySelectorAll('.section');
+    const sections = document.querySelectorAll('section');
     const scrollPos = window.scrollY || document.documentElement.scrollTop;
 
     sections.forEach(section => {
         if (scrollPos >= section.offsetTop - 100 && scrollPos < section.offsetTop + section.offsetHeight) {
             document.querySelectorAll('nav ul li a').forEach(a => {
-                a.classList.remove('active');
+                a.style.color = "white";
                 if (section.getAttribute('id') === a.getAttribute('href').substring(1)) {
-                    a.classList.add('active');
+                    a.style.color = "#ff6347";
                 }
             });
         }
